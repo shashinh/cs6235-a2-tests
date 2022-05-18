3adddc1135d144dcc40b19796a9c2007ecf710f9
Shashin Halalingaiah <shashin.halalingaiah@gmail.com>
2022-05-18 08:43:57 +0530

    added test24

---
+++ test24/P.java
@@ -0,0 +1,62 @@
+//nodes should not be killed when a non-singleton thread object is joined
+class P {
+	public static void main(String [] args){
+			T1 t1_or_t2;
+			O o;
+			boolean b;
+		try {
+			o = new O();
+			b = o.getBool();;
+
+			if(b) {
+				t1_or_t2 = new T1();
+			} else {
+				t1_or_t2 = new T2();
+			}
+			
+			t1_or_t2.start();
+			t1_or_t2.join();
+			o.qm();
+
+
+		} catch (Exception ex) {
+		}
+	}
+}
+
+class T1 extends Thread {
+
+	public void run() {
+		T1 t1;
+		t1 = this;
+
+		t1.qm1();
+	}
+
+	public void qm1() {}
+
+}
+
+class T2 extends T1 {
+
+	public void run() {
+		T2 t2;
+		t2 = this;
+
+		t2.qm1();
+	}
+
+	public void qm1() {}
+
+}
+
+class O {
+
+	public boolean getBool() {
+		boolean ret;
+		ret = true;
+		return ret;
+	}
+	public void qm() { }
+}
+
